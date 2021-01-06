84e1da4f7ce893490c4bf5608cf31fddc6cfade0
icebox827 <icebox827@gmail.com>
2021-01-06 13:23:34 -0500

    fix linter errors

--- enum.rb
+++ enum.rb
@@ -106,11 +106,9 @@ module Enumerable
   end
 end
 
-
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/PerceivedComplexity
 # rubocop:enable Style/CaseEquality
-
 
 def multiply_els(arr)
   arr.my_inject(:*)
